d04168dba11d9b70f4937c24e6e4989ac3f642be
TEAM ISTKHAR <mdistkhar1078@gmail.com>
2024-08-10 22:48:11 +0530

    Update start.py

--- BrandrdXMusic/plugins/bot/start.py
+++ BrandrdXMusic/plugins/bot/start.py
@@ -83,8 +83,8 @@ async def start_pm(client, message: Message, _):
                 )
     else:
         out = private_panel(_)
-        await message.reply_Photo(
-            Photo="https://telegra.ph/file/c93bd35ae1c6a0ffbfb4d.jpg",
+        await message.reply_video(
+            video="https://graph.org/file/967c7d074685e1ef93462.mp4",
             caption=_["start_2"].format(message.from_user.mention, app.mention),
             reply_markup=InlineKeyboardMarkup(out),
         )
